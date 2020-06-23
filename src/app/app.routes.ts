f27afef16eeb9d010694e3206e72c807dc15f811
Cristian <cmpg@software-b.com>
2020-06-22 19:25:30 -0500

    Modificaciones en los modulos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,28 +1,11 @@
 import { RouterModule , Routes } from '@angular/router';
 
-
-import { PagesComponent } from './pages/pages.component';
-
-
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
 
 const appRoutes: Routes = [
-    {
-      path: '',
-      component: PagesComponent,
-      children: [
-        { path: 'dashboard', component: DashboardComponent},
-        { path: 'progress', component: ProgressComponent},
-        { path: 'graficas1', component: Graficas1Component},
-        { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-      ]
-    },
     { path: 'register', component: RegisterComponent},
     { path: 'login', component: LoginComponent},
     { path: '**', component: NopagefoundComponent},
